d0f868093c9d49e79b2e7ca4e631ab43b4cca3dd
Ismael Moreira <imcalazans@gmail.com>
2022-09-25 11:12:15 -0300

    Product-OrderItem one-to-many association - part 2

--- src/main/java/com/educandowe/course/entities/Order.java
+++ src/main/java/com/educandowe/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.educandowe.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.educandowe.course.entities.enums.OrderStatus;
@@ -34,6 +37,9 @@ public class Order implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> Items = new HashSet<>();
 	
 	public Order() {	
 	}
@@ -77,6 +83,10 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public Set<OrderItem> getOrderItem(){
+		return Items;
 	}
 
 	@Override
--- src/main/java/com/educandowe/course/entities/OrderItem.java
+++ src/main/java/com/educandowe/course/entities/OrderItem.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import com.educandowe.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name= "tb_order_item")
@@ -15,7 +16,7 @@ public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	
 	private Integer Quantity;
 	private Double Price;
@@ -30,6 +31,7 @@ public class OrderItem implements Serializable {
 		id.setProduct(product);
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
