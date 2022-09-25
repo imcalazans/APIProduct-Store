e116e784a5c5f3199d9bab4681f337b93299d44e
Ismael Moreira <imcalazans@gmail.com>
2022-09-25 10:37:43 -0300

    Product-OrderItem one-to-many association - part 1

---
+++ src/main/java/com/educandowe/course/entities/OrderItem.java
@@ -0,0 +1,65 @@
+package com.educandowe.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.educandowe.course.entities.pk.OrderItemPK;
+
+@Entity
+@Table(name= "tb_order_item")
+public class OrderItem implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private OrderItemPK id;
+	
+	private Integer Quantity;
+	private Double Price;
+	
+	public OrderItem() {	
+	}
+
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		Quantity = quantity;
+		Price = price;
+		id.setOrder(order);
+		id.setProduct(product);
+	}
+	
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order1) {
+		id.setOrder(order1);
+	} 
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product1) {
+		id.setProduct(product1);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(Price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(Price, other.Price);
+	}
+}
---
+++ src/main/java/com/educandowe/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,42 @@
+package com.educandowe.course.entities.pk;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.educandowe.course.entities.Order;
+import com.educandowe.course.entities.Product;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+	
+	
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+	
+	
+	
+
+}
